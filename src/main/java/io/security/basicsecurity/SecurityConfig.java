e7b242e471dd8b31cef5f3746ccdd6d6d2964343
Judalm Park (dami) <wnekfa1004@naver.com>
2023-06-29 19:53:29 +0900

    Form Login 인증

--- src/main/java/io/security/basicsecurity/SecurityConfig.java
+++ src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -5,6 +5,27 @@ import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ *
+ * 폼 로그인 방식
+ * http.formLogin()
+ *                 .loginPage(“/login.html")   				// 사용자 정의 로그인 페이지
+ *                 .defaultSuccessUrl("/home)				// 로그인 성공 후 이동 페이지
+ * 	                .failureUrl(＂/login.html?error=true“)		// 로그인 실패 후 이동 페이지
+ *                 .usernameParameter("username")			// 아이디 파라미터명 설정
+ *                 .passwordParameter(“password”)			// 패스워드 파라미터명 설정
+ *                 .loginProcessingUrl(“/login")			// 로그인 Form Action Url
+ *                 .successHandler(loginSuccessHandler())		// 로그인 성공 후 핸들러
+ *                 .failureHandler(loginFailureHandler())		// 로그인 실패 후 핸들러
+ */
 
 @Configuration
 @EnableWebSecurity// 웹보안 활성화 시 명시 필수
@@ -18,6 +39,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated();  // 인가 정책
         http
                 .formLogin()
-                ;// 인증 정책
+                .loginPage("/loginPage")// 로그인 페이지 url
+                .defaultSuccessUrl("/")//인증 성공시
+                .failureUrl("/login") //실패시 이동 url
+                .usernameParameter("userId") // default = username
+                .passwordParameter("passwd") // default = password
+                .loginProcessingUrl("/login_proc")//from 태그의 action url
+                .successHandler((request, response, authentication) -> { // 성공 핸들러
+                    System.out.println("authentication.getName() = " + authentication.getName());
+                    response.sendRedirect("/");
+                })
+                .failureHandler((request, response, exception) -> { // 실패 핸들러
+                    System.out.println("exception.getMessage() = " + exception.getMessage());
+                    response.sendRedirect("/login");
+                })
+                .permitAll()
+        ;// 인증 정책
     }
 }
