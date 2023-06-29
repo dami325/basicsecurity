d45a4c6d617c50f899a3cc293552e57633aa16cc
Judalm Park (dami) <wnekfa1004@naver.com>
2023-06-29 19:29:43 +0900

    시큐리티 내부 라이브러리 configure 구조 파악 시큐리티 아이디 패스워드 설정 방법

---
+++ src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -0,0 +1,23 @@
+package io.security.basicsecurity;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity// 웹보안 활성화 시 명시 필수
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated();  // 인가 정책
+        http
+                .formLogin()
+                ;// 인증 정책
+    }
+}
